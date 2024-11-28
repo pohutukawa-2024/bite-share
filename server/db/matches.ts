a1e8650ca18e71d8a812e64afe96057f0fcc2d38
Henry Tran <henrytran089@gmail.com>
2024-11-28 21:58:38 +1300

    complete server-side GET matches

--- server/db/matches.ts
+++ server/db/matches.ts
@@ -3,12 +3,18 @@ import db from './connection'
 
 export async function getMatches(id: string) {
   const results = await db('matches')
-    .select()
+    .select(
+      'giver_id as giverId',
+      'receiver_id as receiverId',
+      'status as status',
+      'created_at as createdAt',
+      'updated_at as updatedAt',
+    )
     .where('status', 'active')
     .andWhere((builder) =>
       builder.where('giver_id', id).orWhere('receiver_id', id),
     )
-  return results
+  return results as Match[]
 }
 
 export async function addMatch(match: Match) {
