aefa7201e1aff45dc9fedd3f0e93f122e3e3aebb
Henry Tran <henrytran089@gmail.com>
2024-11-30 14:51:13 +1300

    add some template styling on chatbox

--- client/Pages/MainPage/MatchesPage.tsx
+++ client/Pages/MainPage/MatchesPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { MatchBubble } from '../../../models/matches'
 import ChatBubble from '../../components/MessageBoard/ChatBubble'
 import useGetMatches from '../../hooks/useGetMatches'
@@ -7,8 +8,20 @@ function MatchesPage() {
   const { data, isLoading, isError } = useGetMatches()
   const { user } = useAuth0()
 
+  // selectMatch, which will change when a button in ChatBubble is clicked
+  const [selectMatch, setSelectMatch] = useState({
+    matchesId: 0,
+  })
+  const handleClick = async ({ matchId }: { matchId: number }) => {
+    setSelectMatch(() => {
+      return { matchesId: matchId }
+    })
+  }
+
   if (isLoading) return <div>loading...</div>
   if (isError) return <div>Sorry! An error has occurred.</div>
+
+  console.log(selectMatch.matchesId)
 
   // Creates an array of conversations with the other user, no matter if they were the giver or receiver
   const transformedData: MatchBubble[] = data
@@ -29,14 +42,24 @@ function MatchesPage() {
       })
     : []
 
-  console.log(transformedData)
-
   return (
     <div className="m-16 flex justify-center">
       <div className="flex w-3/5 ">
-        <ChatBubble data={transformedData} />
-        <section className="w-4/5 bg-gray-100">
-          <p>This will be the ChatBox</p>
+        <ChatBubble data={transformedData} onClick={handleClick} />
+        <section className="w-4/5 rounded-md bg-gray-100">
+          <h1 className="text-center">Chat with Other Person</h1>
+          <div>
+            <div className="flex justify-end">
+              <p className="pb-21 m-2 rounded-md bg-green-400 pl-3 pr-3 pt-1">
+                Hello
+              </p>
+            </div>
+            <div className="flex justify-start">
+              <p className="m-2 rounded-md bg-gray-300 pb-1 pl-3 pr-3 pt-1">
+                Yo
+              </p>
+            </div>
+          </div>
         </section>
       </div>
     </div>
--- client/components/MessageBoard/ChatBubble.tsx
+++ client/components/MessageBoard/ChatBubble.tsx
@@ -2,17 +2,20 @@ import { MatchBubble } from '../../../models/matches'
 
 interface Props {
   data: MatchBubble[]
+  onClick: (_: { matchId: number }) => void
 }
 
-export default function ChatBubble({ data }: Props) {
-  console.log(data)
+export default function ChatBubble({ data, onClick }: Props) {
   return (
     <section className="w-1/5">
       <p>Your matches</p>
       <ul>
         {data.map((match) => (
           <li key={`${match.matchId}`}>
-            <button className="m-3 h-24 w-24 rounded-full bg-gray-50">
+            <button
+              onClick={() => onClick({ matchId: match.matchId })}
+              className="m-3 h-24 w-24 rounded-full bg-gray-50"
+            >
               {match.otherUsername}
             </button>
           </li>
