645f2232133fac55eb147a4d31621aafdba2cb47
Henry Tran <henrytran089@gmail.com>
2024-12-02 15:13:59 +1300

    add 2 more users DB function tests

--- server/db/users.test.ts
+++ server/db/users.test.ts
@@ -2,7 +2,12 @@
 
 import { describe, it, expect, beforeAll, beforeEach } from 'vitest'
 import db from './connection'
-import { getUser, updateUserByUsername } from './users'
+import {
+  getUser,
+  getUserByUsername,
+  updateUserByUsername,
+  upsertProfile,
+} from './users'
 
 beforeAll(async () => {
   await db.migrate.latest()
@@ -12,9 +17,23 @@ beforeEach(async () => {
   await db.seed.run()
 })
 
-describe('DB users', () => {
-  it("should return a user's profile", async () => {
+describe('users DB functions', () => {
+  it("should return own user's profile", async () => {
     const user = await getUser('auth0|67476f8defa904b4cd515493')
+    expect(user.username).toBe('superhenry')
+    expect(user).toHaveProperty('id')
+    expect(user).toHaveProperty('username')
+    expect(user).toHaveProperty('fullName')
+    expect(user).toHaveProperty('email')
+    expect(user).toHaveProperty('points')
+    expect(user).toHaveProperty('location')
+    expect(user).toHaveProperty('icon')
+  })
+
+  it("get another user's profile based on their username", async () => {
+    const username = 'slatch'
+    const user = await getUserByUsername(username)
+    expect(user.id).toBe('auth0|6747d7830ba06e401cd48bdb')
     expect(user).toHaveProperty('id')
     expect(user).toHaveProperty('username')
     expect(user).toHaveProperty('fullName')
@@ -32,5 +51,20 @@ describe('DB users', () => {
     await updateUserByUsername(profile)
     const user = await getUser('auth0|67476f8defa904b4cd515493')
     expect(user.username).toBe('averagehenry')
+  })
+
+  it('should insert a new user if not already present', async () => {
+    const newUser = {
+      id: 'auth0|99',
+      username: 'cookies4me',
+      fullName: 'Cookie Monster',
+      email: 'cookies.are.my.thing@cookies.com',
+      points: 0,
+      location: 'sesame street',
+      icon: '',
+    }
+    await upsertProfile(newUser)
+    const user = await getUser('auth0|99')
+    expect(user.username).toBe('cookies4me')
   })
 })
