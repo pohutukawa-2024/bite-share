ffc6cd80e3e887d1a33ad0da7af7ff9c890c28fd
Henry Tran <henrytran089@gmail.com>
2024-12-03 11:56:23 +1300

    set up storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,24 @@
 import type { StorybookConfig } from '@storybook/react-vite'
-
 const config: StorybookConfig = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
+  stories: ['../client/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
-    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: false,
+      },
+    },
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  docs: {
+    autodocs: 'tag',
   },
 }
 export default config
