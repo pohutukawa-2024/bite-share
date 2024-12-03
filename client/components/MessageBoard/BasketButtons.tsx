577cd79d33d980394840a21e8e88badeed567367
harpreet slatch <blueslatch90@gmail.com>
2024-12-03 16:55:08 +1300

    latest match file

--- client/components/MessageBoard/BasketButtons.tsx
+++ client/components/MessageBoard/BasketButtons.tsx
@@ -43,13 +43,13 @@ export default function BasketButtons({
       <div className="mr-2 mt-2 flex gap-2">
         <button
           onClick={handleRelistClick}
-          className=" h-14 w-32 rounded-bl-xl rounded-br-xl bg-[#1f2937]  text-white shadow-md transition duration-200 hover:scale-105 hover:bg-rose-600 hover:text-white focus:outline-none"
+          className="h-[60px] w-32 rounded-bl-xl rounded-br-xl bg-[#1f2937]  text-white shadow-md transition duration-200 hover:scale-105 hover:bg-rose-600 hover:text-white focus:outline-none"
         >
           Re-list <br /> Basket
         </button>
         <button
           onClick={handleCompleteClick}
-          className="h-14 w-32 rounded-bl-xl rounded-br-xl bg-yellow-500 text-black  shadow-md transition duration-200 hover:scale-105 hover:bg-green-600 hover:text-white"
+          className="h-[60px] w-32 rounded-bl-xl rounded-br-xl bg-yellow-500 text-black  shadow-md transition duration-200 hover:scale-105 hover:bg-green-600 hover:text-white"
         >
           Complete <br /> Match
         </button>
